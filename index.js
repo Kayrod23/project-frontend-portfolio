09223ef264f968c3c7547b985798d4bc89c885be
Keeanu Rodriguez <keeanu7@gmail.com>
2023-01-13 23:45:58 -0500

    main page javascript finished

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const BASE_URL = "https://free-to-play-games-database.p.rapidapi.com/api/games"
-const platform = "?platform="
 
-const article = document.querySelector("article");
 const articles = document.querySelectorAll("article");
 const main = document.querySelector("main");
 const form = document.querySelector("form"); 
@@ -20,56 +18,48 @@ fetch(BASE_URL, options) // calls a fetch on website load to show images of game
         createRecentGames(results);
         form.addEventListener("submit", (event) => {
             event.preventDefault();
-            const articles2 =  document.querySelectorAll("article")
+            const articles2 =  document.querySelectorAll("article");
             for (let i=0; i<articles2.length; i++){ // removes articles on load
                 articles2[i].style.display = "none";
             }
             const userInput = document.querySelector("input").value;
-            getGamesByID(results, userInput);
+            getGamesBySearch(results, userInput);
             form.reset();
         })
     })
 	.catch(err => console.error(err));
 
-
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     const userInput = document.querySelector("input").value;
-//     getGamesByID(results, userInput);
-//     form.reset();
-// })
-
-// function loadRecentGame(){ // fetches info from api
-//     fetch(`${BASE_URL}`)
-//     .then((response) => response.json())
-//     .then((results) => {
-//         createRecentGames(results);
-//         // previousSearches(result, userInput);
-//     })
-//     .catch((error) => console.log(error));
-//     }
-
 function createRecentGames(results){
     for (let i=0; i<articles.length; i++){
         articles[i].style.backgroundImage = "url("+results[i].thumbnail+")";
         articles[i].addEventListener("click", (event) => {
-            userInput = event.target
-            // getGamesByID(results, userInput);
-// takes you to another html page with more details on the game
-            const p = document.createElement("p");
-            p.innerHTML = "here"
-            main.append(p)
-            })
+        sessionStorage.setItem("articleClicked", event.target.style.backgroundImage);
+        location.replace("Game.html");
+        })
     }
 }
 
-function getGamesByID(results, userInput){ // finds games based on userinput and turns it into an article
+function getGamesBySearch(results, userInput){ // finds games based on userinput and turns it into an article
     for(const result of results){
       if(result.title.toLowerCase().includes(userInput.toLowerCase()) || result.genre.toLowerCase().includes(userInput.toLowerCase())) {
         const article = document.createElement("article");
         article.style.backgroundImage = "url("+result.thumbnail+")";
-        main.append(article)
+        article.addEventListener("click", (event) => {
+            sessionStorage.setItem("articleClicked", event.target.style.backgroundImage);
+            location.replace("Game.html");
+        })
+        main.append(article);
       }
     }   
 }
-// loadRecentGame()
+
+const logo = document.querySelector("img");
+logo.addEventListener("click", () => {
+    location.replace("index.html");
+})
+
+if(localStorage > 0){
+    for (let i=0; i<localStorage.length; i++){
+        
+    }
+}
